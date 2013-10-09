803e041d78a6b89e5d89934ef3b61bb0ca5dd171
augiegardner <augdog911@gmail.com>
2013-10-09 12:08:29 -0400

    changed final page to use table CSS/HTML

--- assets/www/final.php
+++ assets/www/final.php
@@ -1,16 +1,20 @@
 <!-- Begin Final Page -->
 
 <div class="calls-saved">
-	<div class="calls-saved-container">
-		<div class="calls-saved-num">
-			0
-		</div>
-		<span>Calls Saved</span>
-		<div class="icons-final clearfix smallsep">
-			<a id="twtShareLink"><div class="twitter-final" >&nbsp;</div></a>
-			<div class="facebook-final">&nbsp;</div>
-		</div>
-	</div>
+	<table class="calls-saved-table">
+		<tr>
+			<td>
+				<div class="calls-saved-num">
+					0
+				</div>
+				<span>Calls Saved</span>
+				<div class="icons-final clearfix smallsep">
+					<a id="twtShareLink"><div class="twitter-final" >&nbsp;</div></a>
+					<div class="facebook-final">&nbsp;</div>
+				</div>
+			</td>
+		</tr>
+	</table>
 </div>
 
 <!-- End Final Page -->
